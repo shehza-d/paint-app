d46fbbc1e56741771337c99a091a1472c7cfb145
Hiten Kumar <HitenKumar786>
2022-08-05 23:10:23 +0500

    push

--- src/script.js
+++ src/script.js
@@ -1,47 +1,66 @@
+let colorPickingFunction = () => {
+	let colorPicked = document.querySelector('#colorPicker').value;
+	console.log('color', colorPicked)
+	document.querySelector('.align').style.backgroundColor = colorPicked;
+	return colorPicked
+}
+ 
+
 
 let selectColor = (event) => {
 	event.preventDefault()
-	let colorPicked = document.querySelector('#colorPicker').value;
-	console.log(colorPicked)
-	document.querySelector('.align').style.backgroundColor = colorPicked;
+	let colorPicked = colorPickingFunction();
+	console.log('color', colorPicked)
+
+	// colorPicked ? colorPicked: #00000
 
 
+	//sir ka code 
+	let ctx = document.querySelector("#myCanvas").getContext("2d");
+	ctx.fillStyle = `${colorPicked}`
+	// ctx.fillRect(0, 0, 100, 100);
 
-//sir ka code 
-let ctx = document.querySelector("#myCanvas").getContext("2d");
-ctx.fillStyle =  `${colorPicked}`
-// ctx.fillRect(0, 0, 100, 100);
+	let isMouseDown = false;
 
-let isMouseDown = false;
+	document.querySelector("#myCanvas").addEventListener("mousedown", (event) => {
+		isMouseDown = true;
+	});
+	document.querySelector("#myCanvas").addEventListener("mouseup", (event) => {
+		isMouseDown = false;
+	});
 
-document.querySelector("#myCanvas").addEventListener("mousedown", (event) => {
-	isMouseDown = true;
-});
-document.querySelector("#myCanvas").addEventListener("mouseup", (event) => {
-	isMouseDown = false;
-});
+	document.querySelector("#myCanvas").addEventListener("mousemove", (event) => {
 
-document.querySelector("#myCanvas").addEventListener("mousemove", (event) => {
+		if (isMouseDown) {
+			// console.log("move: ", event);
+			console.log("h: ", event.offsetX);
+			console.log("w: ", event.offsetY);
 
-	if (isMouseDown) {
-		// console.log("move: ", event);
-		console.log("h: ", event.offsetX);
-		console.log("w: ", event.offsetY);
+			ctx.fillRect(
+				event.offsetX,
+				event.offsetY,
+				2, 2
+			);
 
-		ctx.fillRect(
-			event.offsetX,
-			event.offsetY,
-			2, 2
-		);
+		}
 
-	}
-
-});
+	});
 }
-let downloadCanvas =()=>{
-	let canvas =document.querySelector('#myCanvas');
-	let anchor=document.createElement('a');
-	anchor.href=canvas.toDataURL('image/jpg');
-	anchor.download='shehzad_paint_app.jpg';
+
+let downloadCanvas = () => {
+	let canvas = document.querySelector('#myCanvas');
+	let anchor = document.createElement('a');
+	anchor.href = canvas.toDataURL('image/jpg');
+	anchor.download = 'shehzad_paint_app.jpg';
 	anchor.click();
 }
+
+/*
+05-aug-2022
+let textFormDB = ""
+clrt+D = edit all variable ()
+
+difference b/t display none and visibility hidden
+
+
+*/
